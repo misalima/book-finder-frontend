45e95334b379ef6384ce3f332715df430431b4d5
Misael <misael.alisson14@gmail.com>
2024-08-24 19:27:48 -0300

    set Footer background to black, and added button for Cadastrar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,11 +1,24 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function NavBar() {
   return (
     <header className="z-50 px-40 h-24 w-full bg-primary-green flex items-center justify-between">
-        <Image className='max-h-[60px] cursor-pointer' alt='logo' width={160} height={60} src={'/images/logo.png'}></Image>
-        <button className='bg-white px-8 py-2 rounded-lg text-lg font-medium text-primary-green hover:bg-[#dddddd]'>Entrar</button>
+      <Image
+        className="max-h-[60px] cursor-pointer"
+        alt="logo"
+        width={160}
+        height={60}
+        src={"/images/logo.png"}
+      ></Image>
+      <div className="flex gap-3">
+        <button className="bg-white w-28 py-2 rounded-lg text-lg font-medium text-primary-green hover:bg-[#dddddd]">
+          Entrar
+        </button>
+        <button className="ring-2 ring-white ring-inset bg-primary-green w-28 py-2 rounded-lg text-lg font-medium text-white hover:bg-secondary-green">
+          Cadastrar
+        </button>
+      </div>
     </header>
-  )
+  );
 }
