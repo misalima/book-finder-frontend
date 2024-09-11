a6663f58ac3b2bbf9d07e3eccdc7dbc5d07f34b9
smili1605 <joaoguilhermesilvasouza@gmail.com>
2024-09-11 16:32:56 -0300

    Logic settings for confirming password and public button filled in #31

--- src/components/FormEditProfile/index.tsx
+++ src/components/FormEditProfile/index.tsx
@@ -1,9 +1,7 @@
 "use client";
 import { useForm } from "react-hook-form";
 import React, { useState, useEffect } from "react";
-import { yupResolver } from "@hookform/resolvers/yup";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
-import * as yup from "yup";
 import { IUser } from "@/types/user";
 
 export interface FormEditProfile {
@@ -14,38 +12,31 @@ export interface FormEditProfile {
   newPassword: string;
 }
 
-const schema = yup.object({
-  username: yup.string().required("Nome de usuário é obrigatório"),
-  email: yup.string().email("E-mail inválido").required("E-mail é obrigatório"),
-  password: yup
-    .string()
-    .min(8, "A senha deve ter no mínimo 8 caracteres")
-    .required("Senha é obrigatória"),
-  newPassword: yup.string().required("Senha é obrigatória"),
-  confirmPassword: yup
-    .string()
-    .oneOf([yup.ref("newPassword")], "As senhas devem coincidir")
-    .required("Confirmação de senha é obrigatória"),
-}).required();
-
 export default function FormRegistration(user: IUser) {
   const {
     register,
     handleSubmit,
-    formState: { errors },
     watch,
-    setValue, 
+    setValue,
+    formState: { isDirty }
   } = useForm<FormEditProfile>({
-    resolver: yupResolver(schema),
+    defaultValues: {
+      username: user.username,
+      email: user.email
+    }
   });
 
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [selected, setSelected] = useState<string | null>(null);
-  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [selected, setSelected] = useState<string | null>("publico");
+
+  const currentPassword = watch("password");
+  const username = watch("username");
+  const email = watch("email");
+  const newPassword = watch("newPassword");
+  const confirmPassword = watch("confirmPassword");
 
   useEffect(() => {
-   
     setValue("username", user.username);
     setValue("email", user.email);
   }, [user, setValue]);
@@ -66,32 +57,25 @@ export default function FormRegistration(user: IUser) {
     console.log(data);
   };
 
-  useEffect(() => {
-    const subscription = watch((value, { name, type }) => {
-      setIsButtonDisabled(false);
-    });
-    return () => subscription.unsubscribe();
-  }, [watch]);
+  const isButtonDisabled = !(
+    isDirty &&
+    (username !== user.username || email !== user.email ||
+     newPassword || confirmPassword)
+  );
 
   return (
     <div className="flex justify-center items-center py-6 px-96 ">
-      <form className=" w-full space-y-4" onSubmit={handleSubmit(onSubmit)}>
+      <form className="w-full space-y-4" onSubmit={handleSubmit(onSubmit)}>
         <span className="text-white font-bold text-4xl">Editar Perfil</span>
+
         <div>
-          <label className="font-medium ml-1 text-white ">Usuário</label>
+          <label className="font-medium ml-1 text-white">Usuário</label>
           <input
             id="username"
             type="text"
             className="rounded-lg w-full p-2 focus:outline-none border border-gray-400"
             {...register("username")}
-            
           />
-
-          {errors.username && (
-            <div className="ml-1 error-message text-errors font-semibold">
-              {errors.username.message}
-            </div>
-          )}
         </div>
 
         <div>
@@ -101,16 +85,11 @@ export default function FormRegistration(user: IUser) {
             type="email"
             className="rounded-lg w-full p-2 focus:outline-none border border-gray-400"
             {...register("email")}
-          
           />
-          {errors.email && (
-            <div className="ml-1 error-message text-errors font-semibold">
-              {errors.email.message}
-            </div>
-          )}
         </div>
+
         <div className="relative">
-          <label className="font-medium ml-1 text-white ">Senha Atual</label>
+          <label className="font-medium ml-1 text-white">Senha Atual</label>
           <input
             id="password"
             type={showPassword ? "text" : "password"}
@@ -118,54 +97,44 @@ export default function FormRegistration(user: IUser) {
             {...register("password")}
           />
           <div
-            className={`absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer ${errors.password ? "" : "pt-5"
-              }`}
+            className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
             onClick={togglePasswordVisibility}
           >
             {showPassword ? (
               <AiFillEyeInvisible
-                className="hover:text-dark-grey"
+                className="hover:text-dark-grey mt-5"
                 size={30}
               />
             ) : (
-              <AiFillEye className="hover:text-dark-grey" size={30} />
+              <AiFillEye className="hover:text-dark-grey mt-5" size={30} />
             )}
           </div>
-          {errors.password && (
-            <div className="ml-1 error-message text-errors font-semibold">
-              {errors.password.message}
-            </div>
-          )}
         </div>
 
         <div className="relative">
-          <label className="font-medium ml-1 text-white ">Nova Senha</label>
+          <label className="font-medium ml-1 text-white">Nova Senha</label>
           <input
             id="newPassword"
             type={showPassword ? "text" : "password"}
             className="rounded-lg w-full focus:outline-none p-2 border border-gray-400"
             {...register("newPassword")}
+            disabled={!currentPassword}
           />
           <div
-            className={`absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer ${errors.password ? "" : "pt-5"
-              }`}
+            className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
             onClick={togglePasswordVisibility}
           >
             {showPassword ? (
               <AiFillEyeInvisible
-                className="hover:text-dark-grey"
+                className="hover:text-dark-grey mt-5"
                 size={30}
               />
             ) : (
-              <AiFillEye className="hover:text-dark-grey" size={30} />
+              <AiFillEye className="hover:text-dark-grey mt-5" size={30} />
             )}
           </div>
-          {errors.password && (
-            <div className="ml-1 error-message text-errors font-semibold">
-              {errors.password.message}
-            </div>
-          )}
         </div>
+
         <div className="relative">
           <label className="font-medium ml-1 text-white">Confirmar Senha</label>
           <input
@@ -173,27 +142,21 @@ export default function FormRegistration(user: IUser) {
             type={showConfirmPassword ? "text" : "password"}
             className="rounded-lg w-full focus:outline-none p-2 border border-gray-400"
             {...register("confirmPassword")}
+            disabled={!currentPassword}
           />
           <div
-            className={`absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer ${
-              errors.confirmPassword ? "" : "pt-5"
-            }`}
+            className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
             onClick={toggleConfirmPasswordVisibility}
           >
             {showConfirmPassword ? (
               <AiFillEyeInvisible
-                className="hover:text-dark-grey"
+                className="hover:text-dark-grey mt-5"
                 size={30}
               />
             ) : (
-              <AiFillEye className="hover:text-dark-grey" size={30} />
+              <AiFillEye className="hover:text-dark-grey mt-5" size={30} />
             )}
           </div>
-          {errors.confirmPassword && (
-            <div className="ml-1 error-message text-errors font-semibold">
-              {errors.confirmPassword.message}
-            </div>
-          )}
         </div>
 
         <div className="flex justify-end">
@@ -204,7 +167,7 @@ export default function FormRegistration(user: IUser) {
               type="radio"
               checked={selected === "publico"}
               onChange={() => handleCheckboxChange("publico")}
-              className="w-4 h-4  border-gray-300 ml-4"
+              className="w-4 h-4 border-gray-300 ml-4"
             />
             <label className="font-medium ml-2 text-white">Público</label>
           </div>
@@ -215,7 +178,7 @@ export default function FormRegistration(user: IUser) {
               type="radio"
               checked={selected === "privado"}
               onChange={() => handleCheckboxChange("privado")}
-              className="w-4 h-4  ml-4 "
+              className="w-4 h-4 ml-4"
             />
             <label className="font-medium ml-2 text-white">Privado</label>
           </div>
@@ -224,9 +187,7 @@ export default function FormRegistration(user: IUser) {
         <div className="flex justify-end">
           <button
             type="submit"
-            className={`rounded-lg font-medium p-3  text-white 
-              ${isButtonDisabled ? "hover:bg-gray-500 bg-primary-green cursor-not-allowed" : "bg-primary-green hover:bg-green-600"} 
-             `}
+            className={`rounded-lg font-medium p-3 text-white ${isButtonDisabled ? 'bg-gray-400 hover:bg-gray-500' : 'bg-primary-green hover:bg-green-600'}`}
             disabled={isButtonDisabled}
           >
             Salvar Alterações
