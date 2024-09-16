6faf334babcc975b907fbe1cf9a783e695a786ba
Misael Lima <misael.alisson14@gmail.com>
2024-09-16 14:57:37 -0300

    change search button bg color, and center search form

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -21,46 +21,49 @@ const NavBar = () => {
     
 
     return (
-        <header className="z-50 px-8 md:px-40 h-24 w-full bg-primary-green flex items-center justify-between">
-            <Link href="/">
-                <Image
-                    className="max-h-[60px] cursor-pointer"
-                    alt="logo"
-                    width={160}
-                    height={60}
-                    src="/images/logo.png"
-                />
-            </Link>
+      <header className="z-50 px-8 md:px-40 h-24 w-full bg-primary-green flex items-center justify-between">
+        <Link href="/">
+          <Image
+            className="max-h-[60px] cursor-pointer"
+            alt="logo"
+            width={160}
+            height={60}
+            src="/images/logo.png"
+          />
+        </Link>
+        <form onSubmit={handleSearchSubmit} className="flex items-center">
+          <SearchBar />
+          <button
+            type="submit"
+            className="ml-3 bg-gray-300 px-4 py-2 rounded-lg text-lg text-gray-500 hover:bg-tertiary-green"
+          >
+            Buscar
+          </button>
+        </form>
+        <div className="flex items-center space-x-4">
+          {session ? (
+            <UserMenu
+              username={session.user?.name || "User"}
+              id={session.user?.id}
+            />
+          ) : (
             <div className="flex items-center space-x-4">
-                <form onSubmit={handleSearchSubmit} className="flex items-center">
-                    <SearchBar />
-                    <button
-                        type="submit"
-                        className="ml-2 bg-secondary-green px-6 py-2 rounded-lg text-lg font-medium text-white hover:bg-tertiary-green"
-                    >
-                        Buscar
-                    </button>
-                </form>
-                {session ? (
-                    <UserMenu username={session.user?.name || "User"} id={session.user?.id} />
-                ) : (
-                    <div className="flex items-center space-x-4">
-                        <button
-                            className="bg-white mr-4 px-8 py-2 rounded-lg text-lg font-medium text-primary-green hover:bg-[#dddddd]"
-                            onClick={() => router.push("/login")}
-                        >
-                            Entrar
-                        </button>
-                        <button
-                            className="ring-2 ring-white ring-inset bg-primary-green w-28 py-2 rounded-lg text-lg font-medium text-white hover:bg-secondary-green"
-                            onClick={() => router.push("/register")}
-                        >
-                            Cadastrar
-                        </button>
-                    </div>
-                )}
+              <button
+                className="bg-white mr-4 px-8 py-2 rounded-lg text-lg font-medium text-primary-green hover:bg-[#dddddd]"
+                onClick={() => router.push("/login")}
+              >
+                Entrar
+              </button>
+              <button
+                className="ring-2 ring-white ring-inset bg-primary-green w-28 py-2 rounded-lg text-lg font-medium text-white hover:bg-secondary-green"
+                onClick={() => router.push("/register")}
+              >
+                Cadastrar
+              </button>
             </div>
-        </header>
+          )}
+        </div>
+      </header>
     );
 };
 
