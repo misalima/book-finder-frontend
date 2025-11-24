181cd43d9de55cb01cd8de9aaf2cc92463060b69
Misael Lima <misael.alisson14@gmail.com>
2025-11-24 20:15:04 -0300

    fix: update LoadingScreen text to Portuguese and adjust layout for better alignment

--- src/components/LoadingScreen/index.tsx
+++ src/components/LoadingScreen/index.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const LoadingScreen: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="text-white text-center">
+      <div className="text-white flex flex-col items-center">
         <div className="loader ease-linear rounded-full border-8 border-t-8 border-white h-24 w-24 mb-4"></div>
-        <h2 className="text-2xl font-semibold">Loading...</h2>
+        <h2 className="text-2xl font-semibold">Carregando...</h2>
       </div>
     </div>
   );
